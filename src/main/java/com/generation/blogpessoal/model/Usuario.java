941214c49b15c071694f0d16e349189ca8ec412f
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-06-26 12:36:52 -0300

    Update Usuario.java

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -1,92 +1,107 @@
 package com.generation.blogpessoal.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
-import java.util.List;
 
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Este atributo NOME é obrigatório")
+	private String nome;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @NotBlank(message = "O campo Nome é obrigatório")
-    private String nome;
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "Este atributo USUARIO é obrigatório")
+	@Email(message = "O atributo usuario vai receber um EMAIL valído")
+	private String usuario;
+	
+	@NotBlank(message = "Este atributo SENHA é obrigatório")
+	@Size(min = 8, message = "A senha deve ter no minimo 8 caracteres" )
+	private String senha;
+	
+	@Size(max=5000, message = "O Link da foto não pode ser maior que 5000 caracteres")
+	private String foto;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 
-    @Schema(example = "email@email.com.br")
-    @NotBlank(message = "O campo usuario é obrigatório")
-    @Email(message = "O usuario vai receber um e-mail válido")
-    private String usuario;
-
-    @NotBlank(message = "O campo senha é obrigatório")
-    @Size(min = 8, message = "A senha tem que ter no minimo 8 caracteres")
-    private String senha;
-
-    @Size(max = 5000, message = "O link da foto não deve ser maior que 5000 caracteres")
-    private String foto;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
-    @JsonIgnoreProperties("usuario")
-    private List<Postagem> postagem;
-
-    public Usuario() {
-    }
 
     public Usuario(Long id, String nome, String usuario, String senha, String foto) {
-        this.id = id;
-        this.nome = nome;
-        this.usuario = usuario;
-        this.senha = senha;
-        this.foto = foto;
-    }
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	public Usuario() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getUsuario() {
+		return usuario;
+	}
 
-    public String getUsuario() {
-        return usuario;
-    }
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
 
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
+	public String getSenha() {
+		return senha;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
+	public String getFoto() {
+		return foto;
+	}
 
-    public String getFoto() {
-        return foto;
-    }
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
 
-    public void setFoto(String foto) {
-        this.foto = foto;
-    }
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
 
-    public List<Postagem> getPostagem() {
-        return postagem;
-    }
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 }
