541d766b15573f0c5e33b4e05af7ea2934b9dc96
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-06-26 12:07:12 -0300

    Update Tema.java

--- src/main/java/com/generation/blogpessoal/model/Tema.java
+++ src/main/java/com/generation/blogpessoal/model/Tema.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
