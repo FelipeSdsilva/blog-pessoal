10d094eb00fed44fbfc8bb672cfff6a9aac18e41
Felipe Sousa <47900701+FelipeSdsilva@users.noreply.github.com>
2023-06-26 12:34:33 -0300

    Update Postagem.java

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,61 +1,47 @@
 package com.generation.blogpessoal.model;
 
-import java.util.List;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_usuarios")
-public class Usuario {
+@Table(name="tb_postagens")
+
+public class Postagem {
 	
-	@Id
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@NotNull(message = "Este atributo NOME é obrigatório")
-	private String nome;
+	@NotBlank(message  = "Este atributo é de preenchimento obrigatório")
+	@Size(min = 5 , max = 100 , message = "Este campo tem que possuir no minimo 5 caracteres e no maximo 100 caracteres" )
+	private String titulo;
+	
+	@NotBlank(message  = "Este atributo é de preenchimento obrigatório")
+	@Size(min = 10 , max = 1000 , message = "Este campo tem que possuir no minimo 10 caracteres e no maximo 1000 caracteres" )
+	private String texto;
 
-	@Schema(example = "email@email.com.br")
-	@NotNull(message = "Este atributo USUARIO é obrigatório")
-	@Email(message = "O atributo usuario vai receber um EMAIL valído")
-	private String usuario;
+	@UpdateTimestamp
+	private LocalDateTime data;
 	
-	@NotBlank(message = "Este atributo SENHA é obrigatório")
-	@Size(min = 8, message = "A senha deve ter no minimo 8 caracteres" )
-	private String senha;
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
-	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
-		this.id = id;
-		this.nome = nome;
-		this.usuario = usuario;
-		this.senha = senha;
-		this.foto = foto;
-	}
-	
-	public Usuario() {
-		
-	}
-
-	@Size(max=5000, message = "O Link da foto não pode ser maior que 5000 caracteres")
-	private String foto;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "usuario", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("usuario")
-	private List<Postagem> postagem;
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -65,43 +51,43 @@ public class Usuario {
 		this.id = id;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getTitulo() {
+		return titulo;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
 	}
 
-	public String getUsuario() {
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	public Usuario getUsuario() {
 		return usuario;
 	}
 
-	public void setUsuario(String usuario) {
+	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
-	public String getFoto() {
-		return foto;
-	}
-
-	public void setFoto(String foto) {
-		this.foto = foto;
-	}
-
-	public List<Postagem> getPostagem() {
-		return postagem;
-	}
-
-	public void setPostagem(List<Postagem> postagem) {
-		this.postagem = postagem;
 	}
 }
